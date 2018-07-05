aadc0fd4301abdb676478f9e0e528a95c88b25c8
chenhuaping <18770688010@139.com>
2018-07-05 18:58:45 +0800

    implement post#create&destory

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "/jquery-1" => "pages#jquery_1"
   get "/jquery_2" => "pages#jquery_2"
   get "/jquery_3" => "pages#jquery_3"
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   get "/jquery_5" => "pages#jquery_5"
   get "/jquery_6" => "pages#jquery_6"
   get "/jquery_7" => "pages#jquery_7"
-  root "pages#jquery_1"
+  resources :posts
+  root "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
