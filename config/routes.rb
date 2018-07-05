fabe845ee798f1d1b5ef36865b5736600291035c
chenhuaping <18770688010@139.com>
2018-07-05 18:06:01 +0800

    some css practise

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get "/jquery_3" => "pages#jquery_3"
   get "/jquery_4" => "pages#jquery_4"
   get "/jquery_5" => "pages#jquery_5"
+  get "/jquery_6" => "pages#jquery_6"
+  get "/jquery_7" => "pages#jquery_7"
   root "pages#jquery_1"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
