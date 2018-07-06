ce88dd032a7c16b29ffa3f9dd1db82e06bba150c
chenhuaping <18770688010@139.com>
2018-07-06 14:03:15 +0800

    change post#delete with ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,14 +14,14 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy
-
+    render :json => { :id => @post.id }
   end
   def like
     @post = Post.find(params[:id])
     unless @post.find_like(current_user) #如果已经按过赞了，就略过不再新增
       Like.create(:user => current_user, :post => @post)
     end
-    
+
   end
   def unlike
     @post = Post.find(params[:id])
